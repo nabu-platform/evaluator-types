a10bae6420d16d65c67cc9ef3d83be25250307b1
alex <alex@nabu.be>
2016-04-26 09:57:47 +0200

    Allow list access to complex content

--- src/main/java/be/nabu/libs/evaluator/types/accessors/ComplexContentAccessor.java
+++ src/main/java/be/nabu/libs/evaluator/types/accessors/ComplexContentAccessor.java
@@ -1,10 +1,16 @@
 package be.nabu.libs.evaluator.types.accessors;
 
-import be.nabu.libs.evaluator.EvaluationException;
-import be.nabu.libs.evaluator.api.ContextAccessor;
-import be.nabu.libs.types.api.ComplexContent;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-public class ComplexContentAccessor implements ContextAccessor<ComplexContent> {
+import be.nabu.libs.evaluator.EvaluationException;
+import be.nabu.libs.evaluator.api.ListableContextAccessor;
+import be.nabu.libs.types.TypeUtils;
+import be.nabu.libs.types.api.ComplexContent;
+import be.nabu.libs.types.api.Element;
+
+public class ComplexContentAccessor implements ListableContextAccessor<ComplexContent> {
 
 	@Override
 	public Class<ComplexContent> getContextType() {
@@ -19,6 +25,17 @@ public class ComplexContentAccessor implements ContextAccessor<ComplexContent> {
 	@Override
 	public Object get(ComplexContent context, String name) throws EvaluationException {
 		return context.get(name);
+	}
+
+	@Override
+	public Collection<String> list(ComplexContent object) {
+		List<String> names = new ArrayList<String>();
+		if (object.getType() != null) {
+			for (Element<?> element : TypeUtils.getAllChildren(object.getType())) {
+				names.add(element.getName());
+			}
+		}
+		return names;
 	}
 
 }
